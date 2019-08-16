9f2f73756c8c7b55cbf43854525bb4a1bf442434
MukundaHolla <kanda05holla@gmail.com>
2019-08-16 17:22:37 +0530

    Learnt => Hash vs History and active links and Redirecting using .go(1 or -1)

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,6 +4,20 @@
             <li><router-link to="/">Home</router-link></li>
             <li><router-link :to="{ name: 'About'}">About</router-link></li>
         </ul>
+        <h2>User profiles</h2>
+        <ul>
+            <li v-for="(id, index) in userIds" :key="index">
+                <router-link :to="{ name: 'ViewProfile', params: { user_id: id}}">
+                    <span>Profile {{ id }}</span>
+                </router-link>
+            </li>
+        </ul>
+        <h2>Navigation control</h2>
+        <ul>
+            <li><button @click="goBack">Go Back</button></li>
+            <li><button @click="goHome">Redirect to Home</button></li>
+            <li><button @click="goForward">Go Forward</button></li>
+        </ul>
     </nav>
 </template>
 
@@ -12,7 +26,18 @@ export default {
     name: 'Navbar',
     data() {
         return {
-
+            userIds: ['1', '2', '3', '4']
+        }
+    },
+    methods: {
+        goHome() {
+            this.$router.push({ name: 'Home' })
+        },
+        goBack() {
+            this.$router.go(-1)
+        },
+        goForward() {
+            this.$router.go(1)
         }
     }
 }
@@ -28,5 +53,12 @@ export default {
     }
     li {
         display: inline-block;
+        margin: 10px;
+    }
+    span {
+        padding: 0;
+    }
+    a.router-link-exact-active {
+        color: blue;
     }
 </style>
